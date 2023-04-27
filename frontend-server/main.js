7c609bbcc4257e8f7f2eb99f96c86106f4e304cf
Alberto Annunziata <aannunziata@welol.it>
2023-04-27 20:07:08 +0200

    fix

--- frontend-server/main.js
+++ frontend-server/main.js
@@ -12,58 +12,56 @@ const client = new DaprClient();
 
 await client.start();
 
-try {
-  app.get("/healthz", (req, res) => {
-    res.sendStatus(200);
-  });
+app.get("/healthz", (req, res) => {
+  res.sendStatus(200);
+});
 
-  app.listen(port, () => {
-    console.log(`Node subscriber server running on port: ${port}`);
-  });
-  app.post("/review", async (req, res) => {
-    const serviceApp = "Review";
-    const serviceAppMethod = "Create";
+app.listen(port, () => {
+  console.log(`Node subscriber server running on port: ${port}`);
+});
+app.post("/api/review", async (req, res) => {
+  const serviceApp = "review";
+  const serviceAppMethod = "Create";
+
+  const response = await client.invoker.invoke(
+    serviceApp,
+    serviceAppMethod,
+    HttpMethod.POST,
+    req.body
+  );
+
+  return res.send(response);
+});
+
+app.get("/api/geolocator", async (req, res) => {
+  try {
+    const serviceApp = "location";
+    const serviceAppMethod = `around/${req.query.city}/${req.query.lat}/${req.query.lng}`;
 
     const response = await client.invoker.invoke(
       serviceApp,
       serviceAppMethod,
-      HttpMethod.POST,
-      req.body
+      HttpMethod.GET
     );
 
-    return res.send(response);
-  });
+    res.send(response);
+  } catch (err) {
+    console.log(err);
+    res.send(500, err);
+  }
+});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Serve static files
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("/dapr/*", (req, res) => {
+  console.log(req.path);
+});
+// For all other requests, route to React client
+app.get("*", function (req, res) {
+  console.log(req);
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
-  app.get("/api/geolocator", async (req, res) => {
-    try {
-      const serviceApp = "Location";
-      const serviceAppMethod = `around/${req.query.city}/${req.query.lat}/${req.query.lng}`;
-
-      const response = await client.invoker.invoke(
-        serviceApp,
-        serviceAppMethod,
-        HttpMethod.GET
-      );
-
-      res.send(response);
-    } catch (err) {
-      console.log(err);
-      res.send(500, err);
-    }
-  });
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  // Serve static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("/dapr/*", (req, res) => {
-    console.log(req.path);
-  });
-  // For all other requests, route to React client
-  app.get("*", function (req, res) {
-    console.log(req);
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-} catch (e) {
-  console.log(e);
-}
+console.log("Node  App Started");
 export default app;
