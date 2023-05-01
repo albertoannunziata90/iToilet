8da42d26cbe20b40e92450a1e0c9a1c0eca062de
Alberto Annunziata <aannunziata@welol.it>
2023-05-01 09:02:14 +0200

    Remove useless

--- review-api/ReviewApi/Program.cs
+++ review-api/ReviewApi/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddControllers().AddDapr();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDaprClient();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddSingleton(x =>
